9501c9d6d171c13406d30e8502abd4238f58cd74
Bradley Bossard <bradleybossard@gmail.com>
2018-05-11 20:52:37 -0500

    Adding HTML/CSS for tutorial

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,33 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+<div id="app">
+  <div class="cart">
+    <h1 class="title">Order</h1>
+    <ul class="items">
+      <li class="item">
+        <div class="item-preview">
+          <img src="" alt="" class="item-thumbnail">
+          <div>
+            <h2 class="item-title"></h2>
+            <p class="item-description"></p>
+          </div>
+        </div>
+        <div>
+          <input type="text" class="item-quantity">
+          <span class="item-price"></span>
+        </div>
+      </li>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h3 class="cart-line">
+      Subtotal <span class="cart-price"></span>
+    </h3>
+    <h3 class="cart-line">
+      Shipping <span class="cart-price"></span>
+    </h3>
+    <h3 class="cart-line">
+      Total <span class="cart-price cart-total"></span>
+    </h3>
   </div>
+</div>
 </template>
 
 <script>
@@ -31,30 +42,81 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  background: #fdca40;
+  padding: 30px;
 }
-
-h1, h2 {
+.title {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0;
+  text-transform: uppercase;
+  font-size: 110%;
   font-weight: normal;
 }
-
-ul {
-  list-style-type: none;
+.items {
+  margin: 0;
   padding: 0;
+  list-style: none;
 }
-
-li {
-  display: inline-block;
-  margin: 0 10px;
+.cart {
+  background: #fff;
+  font-family: 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  color: #333a45;
+  border-radius: 3px;
+  padding: 30px;
 }
-
-a {
-  color: #42b983;
+.cart-line {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 20px 0 0 0;
+  font-size: inherit;
+  font-weight: normal;
+  color: rgba(51, 58, 69, 0.8);
+}
+.cart-price {
+  color: #333a45;
+}
+.cart-total {
+  font-size: 130%;
+}
+.item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 0;
+  border-bottom: 2px solid rgba(51, 58, 69, 0.1);
+}
+.item-preview {
+  display: flex;
+  align-items: center;
+}
+.item-thumbnail {
+  margin-right: 20px;
+  border-radius: 3px;
+}
+.item-title {
+  margin: 0 0 10px 0;
+  font-size: inherit;
+}
+.item-description {
+  margin: 0;
+  color: rgba(51, 58, 69, 0.6);
+}
+.item-quantity {
+  max-width: 30px;
+  padding: 8px 12px;
+  font-size: inherit;
+  color: rgba(51, 58, 69, 0.8);
+  border: 2px solid rgba(51, 58, 69, 0.1);
+  border-radius: 3px;
+  text-align: center;
+}
+.item-price {
+  margin-left: 20px;
 }
 </style>
